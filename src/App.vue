03427866bfd7b75c98c132f4d26edaef96954159
sokentaku <2748443750@qq.com>
2024-06-24 00:10:56 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- <element-view></element-view> -->
+    <!-- <emp-view></emp-view> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import EmpView from './views/tlias/EmpView.vue'
+/* import ElementView from './views/element/ElementView.vue' */
+export default {
+  // components: {EmpView /* ElementView */ },
+  data() {
+    return {
+      message: "hello world"
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+<style>
+
+</style>
